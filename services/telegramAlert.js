a206c7faa3833072fdc1c17e3e24e5f522918525
Massimiliano Panipucci <info@panipucci.com>
2026-07-01 09:23:16 +0200

    feat: support Telegram FV alert channel

--- services/telegramAlert.js
+++ services/telegramAlert.js
@@ -1,11 +1,10 @@
 const axios = require("axios");
 
-async function sendTelegramMessage(message) {
+async function sendTelegramMessage(message, chatId = process.env.TELEGRAM_CHAT_ID) {
   const token = process.env.TELEGRAM_BOT_TOKEN;
-  const chatId = process.env.TELEGRAM_CHAT_ID;
 
   if (!token || !chatId) {
-    throw new Error("TELEGRAM_BOT_TOKEN o TELEGRAM_CHAT_ID mancanti nel file .env");
+    throw new Error("TELEGRAM_BOT_TOKEN o CHAT_ID mancanti nel file .env");
   }
 
   const url = `https://api.telegram.org/bot${token}/sendMessage`;
